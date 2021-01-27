3a8fc876747a58fb7d3b20d19059cf2901889fb7
RobbeHer <robbe.heremans.r@gmail.com>
2021-01-27 15:53:43 +0100

    Basics MessageController

--- AngularProjectAPI/Models/TwoHaxxContext.cs
+++ AngularProjectAPI/Models/TwoHaxxContext.cs
@@ -17,12 +17,14 @@ namespace AngularProjectAPI.Models
         public DbSet<User> Users { get; set; }
         public DbSet<Room> Rooms { get; set; }
         public DbSet<Talk> Talks { get; set; }
+        public DbSet<Message> Messages { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<User>().ToTable("Users");
             modelBuilder.Entity<Room>().ToTable("Rooms");
             modelBuilder.Entity<Talk>().ToTable("Talks");
+            modelBuilder.Entity<Message>().ToTable("Messages");
 
             modelBuilder.Entity<Talk>()
       .HasOne(v => v.Talker)
